894c7e79a0a25a49a3ccfc6e42875f4498fb2b4f
omp2020 <om4981@gmail.com>
2021-03-31 17:52:49 +0530

    Sign in Added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,13 @@ import { Redirect } from "react-router-dom"
 import firebase from "firebase"
 
 const Login = () => {
-  const [loginData, setLD] = useState({ username: "", password: "" })
+  const [loginData, setLD] = useState({
+    username: "",
+    password: "",
+    cpassword: "",
+  })
   const [error, setError] = useState()
+  const [signup, setSignup] = useState(false)
 
   const changeVal = (e, field) => {
     let val = e.target.value
@@ -17,6 +22,8 @@ const Login = () => {
       case "password":
         setLD({ ...loginData, password: val })
         break
+      case "cpass":
+        setLD({ ...loginData, cpassword: val })
       default:
         break
     }
@@ -35,65 +42,169 @@ const Login = () => {
       })
       .catch((error) => {
         var errorCode = error.code
-        errorCode === "auth/user-not-found" ? setError(true) : setError(false)
+        errorCode === "auth/user-not-found"
+          ? setError({ status: true, value: "User Not Found" })
+          : setError({ status: false, value: "" })
         var errorMessage = error.message
         console.log(errorCode, errorMessage)
       })
+  }
+
+  const makeSignin = () => {
+    if (loginData.cpassword === loginData.password) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(loginData.username, loginData.password)
+        .then((userCredential) => {
+          // Signed in
+          var user = userCredential.user
+          console.log(user.uid)
+          setError({ status: true, value: "User Login Created Successfully" })
+        })
+        .catch((error) => {
+          var errorCode = error.code
+          var errorMessage = error.message
+          errorCode === "auth/weak-password"
+            ? setError({
+                status: true,
+                value: "The Password is weak (Minimul Length = 6)",
+              })
+            : setError({ status: false, value: "" })
+          errorCode === "auth/invalid-email"
+            ? setError({
+                status: true,
+                value: "The Emai Id is badly formatted",
+              })
+            : setError({ status: false, value: "" })
+          console.log("ErrorCode: ", errorCode)
+          console.log("ErrorMessage", errorMessage)
+          // ..
+        })
+    } else {
+      setError({
+        status: true,
+        value: "Confirm password and Password are different",
+      })
+    }
   }
 
   return (
     <>
       <div className="container mt-5 h-100">
         <div className="row align-self-center h-100">
-          <div className="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            {error ? (
-              <div
-                class="alert alert-warning alert-dismissible fade show align-content-center"
-                role="alert"
+          {error ? (
+            <div
+              class="alert alert-danger alert-dismissible fade show align-content-center offset-sm-5"
+              role="alert"
+            >
+              <strong>{error.value}</strong>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="alert"
+                aria-label="Close"
               >
-                <strong>User not Found</strong>
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="alert"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            ) : (
-              ""
-            )}
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div className="card h-100 card-primary justify-content-center">
               <div className="login-brand align-self-center p-3">
                 <b className="h4">Society Management System</b>
               </div>
-              <div className="card-body">
-                <div className="card-header">
-                  <span>Login</span>
+              {signup ? (
+                <div className="card-body">
+                  <div className="card-header">
+                    <a
+                      className="btn btn-outline-info"
+                      onClick={() => setSignup(false)}
+                    >
+                      <span>Login</span>
+                    </a>
+                    <a
+                      className="btn btn-outline-info offset-sm-5"
+                      onClick={() => setSignup(true)}
+                    >
+                      <span>Sign Up</span>
+                    </a>
+                  </div>
+                  <div className="form-group">
+                    <Field
+                      legend="Email"
+                      itype="text"
+                      type="login"
+                      onChange={(e) => changeVal(e, "username")}
+                      name="username"
+                    />
+                    <Field
+                      legend="Password"
+                      itype="password"
+                      type="login"
+                      onChange={(e) => changeVal(e, "password")}
+                      name="password"
+                    />
+                    <Field
+                      legend="Confirm Password"
+                      itype="password"
+                      type="login"
+                      onChange={(e) => changeVal(e, "password")}
+                      name="password"
+                    />
+                  </div>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => makeSignin()}
+                  >
+                    Signup
+                  </button>
+                  {isLogin ? <Redirect to="/admin" /> : ""}
                 </div>
-                <div className="form-group">
-                  <Field
-                    legend="Username"
-                    itype="text"
-                    type="login"
-                    onChange={(e) => changeVal(e, "username")}
-                    name="username"
-                  />
-                  <Field
-                    legend="Password"
-                    itype="password"
-                    type="login"
-                    onChange={(e) => changeVal(e, "password")}
-                    name="password"
-                  />
-                </div>
+              ) : (
+                <div className="card-body">
+                  <div className="card-header">
+                    <a
+                      className="btn btn-outline-info"
+                      onClick={() => setSignup(false)}
+                    >
+                      <span>Login</span>
+                    </a>
+                    <a
+                      className="btn btn-outline-info offset-sm-5"
+                      onClick={() => setSignup(true)}
+                    >
+                      <span>Sign Up</span>
+                    </a>
+                  </div>
+                  <div className="form-group">
+                    <Field
+                      legend="Email"
+                      itype="text"
+                      type="login"
+                      onChange={(e) => changeVal(e, "username")}
+                      name="username"
+                    />
+                    <Field
+                      legend="Password"
+                      itype="password"
+                      type="login"
+                      onChange={(e) => changeVal(e, "password")}
+                      name="password"
+                    />
+                  </div>
 
-                <button className="btn btn-primary" onClick={() => makeLogin()}>
-                  Login
-                </button>
-                {isLogin ? <Redirect to="/admin" /> : ""}
-              </div>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => makeLogin()}
+                  >
+                    Login
+                  </button>
+
+                  {isLogin ? <Redirect to="/admin" /> : ""}
+                </div>
+              )}
             </div>
           </div>
         </div>
